a563dd3bd0c2dc0ddc775a26635fe1b6a4f6ee1b
Dương <ng.quangduong2003@gmail.com>
2025-04-25 22:42:16 +0700

    Revert "Test1"
    This reverts commit 766a346ec037719eef45c82645fc2c3d7a4cd50b.

--- Qltv.java
+++
@@ -1,9 +0,0 @@
-import Asstribute.Controller;
-
-public class Qltv {
-
-    public static void main(String [] args){
-        Controller controller = new Controller();
-        controller.runApp();
-    }
-}
